c1ff2db3d2eea1303589dbd39c053d8bf8ef1909
Manu R <manurajbhar12@gmail.com>
2026-04-14 16:36:23 +0530

    Initial Commit

---
+++ Clipper/ClipperApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClipperApp.swift
+//  Clipper
+//
+//  Created by Manu on 2026-04-14.
+//
+
+import SwiftUI
+
+@main
+struct ClipperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
